1c27b64a1cc78c294243b0a0dc124e14b43f4d0e
Kacper <kacper99@hotmail.co.uk>
2024-02-17 14:52:29 +0000

    feat: initial pages (#2)
    * feat: navbar
    
    * feat: add tailwind
    
    * feat: add base layout, error page, and about
    
    * feat: add pages

---
+++ src/lib/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+	let links = [
+		{ name: 'About', url: '/about' },
+		{ name: 'Experience', url: '/experience' },
+		{ name: 'Projects', url: '/projects' },
+		{ name: 'Blog', url: '/blog' }
+	];
+</script>
+
+<nav class="flex justify-between">
+	<a href="/" class="text-lg font-bold">Kacper Martela</a>
+	<ul class="flex space-x-4">
+		{#each links as link}
+			<li><a href={link.url} class="text-blue-500 hover:text-blue-800">{link.name}</a></li>
+		{/each}
+	</ul>
+</nav>
